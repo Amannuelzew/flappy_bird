b55b587ea39b240abf2d43d599f637b772f3de1b
amanzew <amanzew@gmail.com>
2024-01-05 17:33:53 +0300

    ground scroll

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 pygame.init()
 
 # Set up display
-width, height = 545, 585
+width, height = 545, 620
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("flappy bird")
 
@@ -15,12 +15,19 @@ clock = pygame.time.Clock()
 bg = pygame.image.load('img/bg.png')
 ground = pygame.image.load('img/ground.png')
 
+ground_scroll = 0
+scroll_speed = 4
 # Game loop
 running = True
 while running:
 
+    # daw bg
     screen.blit(bg, (0, 0))
-    screen.blit(ground, (0, 485))
+    # draw ground
+    screen.blit(ground, (ground_scroll, 485))
+    ground_scroll -= scroll_speed
+    if ground_scroll < -35:
+        ground_scroll = 0
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
